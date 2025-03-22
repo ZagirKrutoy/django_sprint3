8252c149917fb24bbf580a03bd1bdfb6a0bc1f9e
Zagir <snuuymoou@gmail.com>
2025-03-23 02:58:39 +0500

    first atempt

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "blog"
+
+urlpatterns = [
+    path("", views.IndexListView.as_view(), name="index"),
+    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("category/<slug:category_slug>/",
+         views.CategoryPostListView.as_view(),
+         name="category_posts"),
+]
---
+++ blogicum/blog/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.utils.timezone import now
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
+from django.shortcuts import get_object_or_404
+
+
+class IndexListView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "post_list"
+    queryset = Post.objects.filter(
+        pub_date__lte=now(),
+        is_published__exact=True,
+        category__is_published=True,
+    ).order_by('-pub_date')[:5]
+
+'''def index(request):
+    template_name = "blog/index.html"
+    inverted_posts = list(reversed(posts))
+    return render(request, template_name, {"posts": inverted_posts})'''
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            pub_date__lte=now(),
+            is_published=True,
+            category__is_published=True
+        )
+
+
+'''def post_detail(request, id):
+    template_name = "blog/detail.html"
+    post = next((post for post in posts if post["id"] == id), None)
+
+    return render(request, template_name, {"post": post})'''
+
+
+class CategoryPostListView(ListView):
+    model = Post
+    template_name = "blog/category.html"
+    context_object_name = "post_list"
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs['category_slug'], is_published=True)
+        return Post.objects.filter(
+            category=category,
+            is_published=True,
+            pub_date__lte=now()
+        ).order_by('-pub_date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category, slug=self.kwargs['category_slug'], is_published=True)
+        return context
+
+
+
+'''def category_posts(request, category_slug):
+    template_name = "blog/category.html"
+    return render(request, template_name, {"category": category_slug})'''
